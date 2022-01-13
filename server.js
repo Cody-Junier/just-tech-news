2230357252e91b2515ff892edaeb6979b5c8093f
cody-junier <codyjunier@gmail.com>
2022-01-12 19:06:18 -0800

    Added comments and upvote functionality

--- server.js
+++ server.js
@@ -24,13 +24,13 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 const routes = require('./controllers');
 app.use(routes);
 
 
-sequelize.sync({ force: true}).then(() =>{
+sequelize.sync({ force: false}).then(() =>{
     app.listen(PORT, () => console.log('Now listening'));
 });
 
